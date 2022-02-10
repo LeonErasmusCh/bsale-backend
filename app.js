7f265c133d96280606ec20c22a3058f447cfbf1a
Leon Erasmus <leondevcode@gmail.com>
2022-02-10 02:01:46 -0300

    .env not working

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
 const mysql = require("mysql");
-const cors = require('cors');
+const cors = require("cors");
 const express = require("express");
 const res = require("express/lib/response");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 // dotenv config
-dotenv.config({ path: './.env' });
+dotenv.config({ path: "./.env" });
 
 const app = express();
-const port = process.env.PORT || 3000 
-app.use(cors());
 
+const port = process.env.PORT || 3000;
+app.use(cors());
 
 // create connection
 
 const db = mysql.createConnection({
- host: process.env.DB_HOST,
+  /*  host: process.env.DB_HOST,
  user: process.env.DB_USER,
  password: process.env.DB_PASSWORD,
- database: process.env.DB_DATABASE, 
-/*  host: "mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com",
-    user: "bsale_test",
-    password: "bsale_test",
-    database: 'bsale_test' */
+ database: process.env.DB_DATABASE,  */
+  host: "mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com",
+  user: "bsale_test",
+  password: "bsale_test",
+  database: "bsale_test",
 });
-
 
 db.connect((err) => {
   if (err) {
-    res.status(500).send("error")
+    res.status(500).send("error");
   }
   console.log("Mysql connected");
 });
@@ -37,9 +36,9 @@ db.connect((err) => {
 app.get("/", (req, res) => {
   let sql = "SELECT * FROM product";
   let query = db.query(sql, (err, results) => {
-    if (err){
-      res.status(500).send("error")
-    };
+    if (err) {
+      res.status(500).send("error");
+    }
     console.log(results);
     res.status(200).send(results);
   });
@@ -49,9 +48,9 @@ app.get("/", (req, res) => {
 app.get("/product/:id", (req, res) => {
   let sql = `SELECT * FROM product WHERE id=${req.params.id}`;
   let query = db.query(sql, (err, results) => {
-    if (err){
-      res.status(500).send("error")
-    };
+    if (err) {
+      res.status(500).send("error");
+    }
     console.log(results);
     res.status(200).send(results);
   });
@@ -62,17 +61,15 @@ app.get("/product/:id", (req, res) => {
 // query string search by category http://localhost:3000/products?category=1
 app.get("/products", (req, res, next) => {
   const search = req.query;
-  console.log("req.query " , search.category)
-  let sql = "SELECT * FROM product WHERE category=" + JSON.parse(search.category);
+  console.log("req.query ", search.category);
+  let sql =
+    "SELECT * FROM product WHERE category=" + JSON.parse(search.category);
   let query = db.query(sql, (err, results) => {
     if (err) throw err;
     console.log(results);
     res.status(200).send(results);
   });
- 
 });
-
-
 
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
